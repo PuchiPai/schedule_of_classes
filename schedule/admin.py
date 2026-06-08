3174b192f508ae733ebfdc1fef1a5dd8a81853a5
yuliya <korepanova.jully@gmail.com>
2026-06-08 13:11:57 +1000

    [p

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import *
 
-admin.site.register(Lesson)
+admin.site.register(Department)
+admin.site.register(Teacher)
+admin.site.register(AcademicGroup)
+admin.site.register(StudentGroup)
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(Discipline)
+admin.site.register(LessonType)
+admin.site.register(Curriculum)
+admin.site.register(TeacherAssignment)
+admin.site.register(TimeSlot)
+admin.site.register(WorkingDay)
+admin.site.register(ScheduleEntry)
